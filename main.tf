d68bf4b62235bd3c28a61caeee7b4d49c0f380c8
Chetan Warad <waradchetan@gmail.com>
2024-11-29 21:48:28 -0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -258,12 +258,12 @@ resource "aws_launch_template" "web_app_launch_template" {
   }
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
-  # Define constants
-  #   SECRET_NAME="rds-db-password"
-  #   REGION="${var.region}"
+ # Define constants
+ #   SECRET_NAME="rds-db-password"
+ #   REGION="${var.region}"
 
-  # Fetch database password from AWS Secrets Manager
-  #   DB_PASSWORD=$(aws secretsmanager get-secret-value --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .password)
+    # Fetch database password from AWS Secrets Manager
+ #   DB_PASSWORD=$(aws secretsmanager get-secret-value --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .password)
 
 
   # User data script for configuring EC2 instance
@@ -974,6 +974,56 @@ resource "aws_route" "private_nat_route" {
   nat_gateway_id         = aws_nat_gateway.nat_gateway.id
 }
 
+#######################################
+# KMS Key Definitions
+#######################################
+
+# KMS Keys for Resources
+resource "aws_kms_key" "ec2_kms" {
+  description         = "KMS key for EC2 encryption"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_key" "rds_kms" {
+  description         = "KMS key for RDS encryption"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_key" "s3_kms" {
+  description         = "KMS key for S3 encryption"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_key" "secrets_manager_kms" {
+  description         = "KMS key for Secrets Manager"
+  enable_key_rotation = true
+}
+
+#######################################
+# Secrets Manager Integration
+#######################################
+
+# Store RDS Password in Secrets Manager
+resource "aws_secretsmanager_secret" "db_password" {
+  name        = "rds-db-password"
+  kms_key_id  = aws_kms_key.secrets_manager_kms.arn
+}
+
+resource "aws_secretsmanager_secret_version" "db_password_version" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = jsonencode({ password = var.db_password })
+}
+
+# Store SendGrid API Key in Secrets Manager
+resource "aws_secretsmanager_secret" "sendgrid_api_key" {
+  name        = "sendgrid-api-key"
+  kms_key_id  = aws_kms_key.secrets_manager_kms.arn
+}
+
+resource "aws_secretsmanager_secret_version" "sendgrid_api_key_version" {
+  secret_id     = aws_secretsmanager_secret.sendgrid_api_key.id
+  secret_string = jsonencode({ api_key = var.sendgrid_api_key })
+}
 
 
 #######################################
